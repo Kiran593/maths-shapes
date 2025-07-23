86e31512747a800c43979b0ff1c7c6d7faae25dd
KIRAN SIVANGI <kiransivangi593@gmail.com>
2025-07-23 06:00:51 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,97 @@
+
+ ## TRIANGLE
+#perimeter of triangle
+
+a=float(input("Enter the length of triangle :"))
+b=float(input("Enter the base of triangle :"))
+c=float(input("Enter the height of triangle :"))
+P= (f"The Perimeter of the triangle:{(a+b+c)}")
+print(P)
+
+
+#area of triangle
+b=float(input("Enter the base of triangle :"))
+h=float(input("Enter the height of triangle :"))
+P= (f"The Area of the triangle:{(b*h)/2}")
+print(P)
+
+
+
+## square
+
+#perimeter of square
+
+a=float(input("Enter the side of square :"))
+P= (f"The Perimeter of the square:{(4*a)}mtr")
+print(P)
+
+#area of square
+
+a=float(input("Enter the side of square :"))
+P= (f"The Area of the square:{a**2}sqmtr")
+print(P)
+
+
+##RECTANGLE
+
+#perimeter of rectangle
+
+a=float(input("Enter the length of rectangle :"))
+b=float(input("Enter the beadth of rectangle:"))
+P= (f"The Perimeter of the rectangle:{2*(a+b)}")
+print(P)
+
+#area of rectangle
+
+a=float(input("Enter the length of rectangle :"))
+b=float(input("Enter the beadth of rectangle:"))
+P= (f"The Area of the rectangle:{(a*b)}")
+print(P)
+
+
+
+##CIRCLE
+
+#diameter of circle
+r=float(input("Enter the radius of circle :"))
+P= (f"The diameter of the circle:{(2*r)}")
+print(P)
+
+#Perimeter of circle
+r=float(input("Enter the radius of circle :"))
+P= (f"The Perimeter of the circle:{(2*r)*22/7}")
+print(P)
+
+#area of the circle
+r=float(input("Enter the radius of circle :"))
+P= (f"The Area of the circle:{22/7*(r**2)}")
+print(P)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
